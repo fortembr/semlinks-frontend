f42964a742e159f7b31c13600608cb3cb4909c29
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-26 23:45:34 -0400

    import and use customTheme

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 // NPM Modules
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Core Modules
 import App from './App';
+import customTheme from 'config/mui.theme';
 import './assets/css/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={customTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
