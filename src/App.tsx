508a44a5a91fe021d78dd5e1b9be5cec2ef1b5bc
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-26 20:36:48 -0400

    move export default directly onto function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './assets/img/logo.svg';
+import './assets/css/App.css';
 
-export default class App extends React.Component {
-  return() {
+export default function App() {
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
