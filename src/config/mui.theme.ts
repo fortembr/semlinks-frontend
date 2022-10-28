d33eb2a0300432caa2c8b263a6bc9845f0535dfd
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-28 00:24:18 -0400

    setup custom breakpoints

--- src/config/mui.theme.ts
+++ src/config/mui.theme.ts
@@ -1,6 +1,15 @@
 import { createTheme } from '@mui/material';
 
 const customTheme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 400,
+      sm: 640,
+      md: 768,
+      lg: 992,
+      xl: 1200
+    }
+  },
   components: {},
   mixins: {},
   palette: {},
