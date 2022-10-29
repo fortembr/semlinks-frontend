6b182ce338007e85befd012df6f2527c48f148a0
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-28 21:39:09 -0400

    replace ts build with yoda template

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,7 @@ module.exports = {
       extends: [
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:prettier/recommended',
-        'plugin:react/recommended'
+        'plugin:prettier/recommended'
       ],
       parser: '@typescript-eslint/parser',
       parserOptions: {
@@ -21,16 +20,21 @@ module.exports = {
       },
       plugins: ['@typescript-eslint/eslint-plugin'],
       rules: {
-        '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unnecessary-type-assertion': 'warn'
+        '@typescript-eslint/no-explicit-any': 'off'
       }
     },
     {
       files: ['*.js'], // Your JavaScript files extension
+      extends: ['plugin:prettier/recommended'],
+      parserOptions: {
+        sourceType: 'module'
+      }
+    },
+    {
+      files: ['/test/**/*.js'], // Your JavaScript files extension
       extends: ['plugin:prettier/recommended'],
       parserOptions: {
         sourceType: 'module'
