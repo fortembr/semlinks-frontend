bae9dd9480b682a44b816db61be53addc6453f8e
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-28 22:21:54 -0400

    delete files element to fix error

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     jest: true
   },
   ignorePatterns: ['.eslintrc.js'],
-  files: ['*.js'], // Your JavaScript files extension
   extends: ['plugin:prettier/recommended'],
   parserOptions: {
     sourceType: 'module'
