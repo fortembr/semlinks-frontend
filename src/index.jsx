d59d94d4a46c83e693f24d44b6b19909f3791417
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-28 22:51:00 -0400

    lint file

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,23 @@
-import React, { Suspense } from "react";
+import React, { Suspense } from 'react';
 
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
-import { Provider } from "react-redux";
-import store from './redux/store'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-import "./assets/icons/remixicon.css";
-import "./assets/less/yoda-theme.less";
+import './assets/icons/remixicon.css';
+import './assets/less/yoda-theme.less';
 
-import App from "./App";
+import App from './App';
 
 ReactDOM.render(
   <Suspense fallback="loading">
     <Provider store={store}>
-      <BrowserRouter >
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
   </Suspense>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
