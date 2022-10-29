ffedc64a31bebbc0d0697786ad2c91f52dff8943
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-28 22:20:19 -0400

    update eslint to fix error

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,10 @@ module.exports = {
     jest: true
   },
   ignorePatterns: ['.eslintrc.js'],
-  overrides: [
-    {
-      files: ['*.js'], // Your JavaScript files extension
-      extends: ['plugin:prettier/recommended'],
-      parserOptions: {
-        sourceType: 'module'
-      }
-    },
-    {
-      files: ['/test/**/*.js'], // Your JavaScript files extension
-      extends: ['plugin:prettier/recommended'],
-      parserOptions: {
-        sourceType: 'module'
-      }
-    }
-  ],
+  files: ['*.js'], // Your JavaScript files extension
+  extends: ['plugin:prettier/recommended'],
+  parserOptions: {
+    sourceType: 'module'
+  },
   root: true
 };
