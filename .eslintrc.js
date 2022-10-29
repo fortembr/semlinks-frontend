6abed0d68755365c17828590709965046518834f
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-28 22:11:29 -0400

    drop typescript from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,27 +6,6 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:prettier/recommended'
-      ],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: ['./tsconfig.json'], // Specify it only for TypeScript files
-        tsconfigRootDir: __dirname,
-        sourceType: 'module'
-      },
-      plugins: ['@typescript-eslint/eslint-plugin'],
-      rules: {
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off'
-      }
-    },
-    {
       files: ['*.js'], // Your JavaScript files extension
       extends: ['plugin:prettier/recommended'],
       parserOptions: {
