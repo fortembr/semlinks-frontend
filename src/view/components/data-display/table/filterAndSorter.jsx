440af10c7c35e364bf2e890b59ab3cdf2baa5786
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-30 20:09:18 -0400

    lint file

--- src/view/components/data-display/table/filterAndSorter.jsx
+++ src/view/components/data-display/table/filterAndSorter.jsx
@@ -100,6 +100,7 @@ export default function FilterAndSorterTable() {
     }
   ];
 
+  // eslint-disable-next-line no-unused-vars
   function onChange(pagination, filters, sorter, extra) {
     // console.log('params', pagination, filters, sorter, extra);
   }
@@ -111,14 +112,14 @@ export default function FilterAndSorterTable() {
           <h4>Filter and sorter</h4>
           <p className="hp-p1-body">
             Use filters to generate filter menu in columns, onFilter to determine filtered result, and filterMultiple to
-            indicate whether it's multiple or single selection. Uses defaultFilteredValue to make a column filtered by
-            default. Use sorter to make a column sortable. sorter can be a function of the type function(a, b){' '}
-            {'{... }'} for sorting data locally. sortDirections: ['ascend' | 'descend'] defines available sort methods
-            for each columns, effective for all columns when set on table props. You can set as ['ascend', 'descend',
-            'ascend'] to prevent sorter back to default status. Uses defaultSortOrder to make a column sorted by
-            default. If a sortOrder or defaultSortOrder is specified with the value ascend or descend, you can access
-            this value from within the function passed to the sorter as explained above. Such a function can take the
-            form: function(a, b, sortOrder) {'{... }'}.
+            indicate whether it&apos;s multiple or single selection. Uses defaultFilteredValue to make a column filtered
+            by default. Use sorter to make a column sortable. sorter can be a function of the type function(a, b){' '}
+            {'{... }'} for sorting data locally. sortDirections: [&apos;ascend&apos; | &apos;descend&apos;] defines
+            available sort methods for each columns, effective for all columns when set on table props. You can set as
+            [&apos;ascend&apos;, &apos;descend&apos;, &apos;ascend&apos;] to prevent sorter back to default status. Uses
+            defaultSortOrder to make a column sorted by default. If a sortOrder or defaultSortOrder is specified with
+            the value ascend or descend, you can access this value from within the function passed to the sorter as
+            explained above. Such a function can take the form: function(a, b, sortOrder) {'{... }'}.
           </p>
         </Col>
 
