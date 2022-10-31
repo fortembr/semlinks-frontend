686c5e1e06a2e1436c55d4d7b94a9dea91f78f18
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-30 20:09:38 -0400

    lint file

--- src/view/components/data-display/table/nestedTable.jsx
+++ src/view/components/data-display/table/nestedTable.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { monoBlue } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import { nested } from "./code.js";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { monoBlue } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { nested } from './code.js';
 
-import { Table, Badge, Menu, Dropdown, Space, Row, Col, Card, Button } from "antd";
-import { RiCodeSSlashLine, RiArrowDownLine } from "react-icons/ri";
+import { Table, Badge, Menu, Dropdown, Space, Row, Col, Card, Button } from 'antd';
+import { RiCodeSSlashLine, RiArrowDownLine } from 'react-icons/ri';
 
 export default function NestedTable() {
   const [checkedCode, setCheckedCode] = useState(false);
@@ -35,7 +35,7 @@ export default function NestedTable() {
             <Badge status="success" />
             Finished
           </span>
-        ),
+        )
       },
       { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
       {
@@ -52,8 +52,8 @@ export default function NestedTable() {
               </a>
             </Dropdown>
           </Space>
-        ),
-      },
+        )
+      }
     ];
 
     const data = [];
@@ -62,7 +62,7 @@ export default function NestedTable() {
         key: i,
         date: '2014-12-24 23:12:00',
         name: 'This is production name',
-        upgradeNum: 'Upgraded: 56',
+        upgradeNum: 'Upgraded: 56'
       });
     }
     return <Table columns={columns} dataSource={data} pagination={false} />;
@@ -75,7 +75,7 @@ export default function NestedTable() {
     { title: 'Upgraded', dataIndex: 'upgradeNum', key: 'upgradeNum' },
     { title: 'Creator', dataIndex: 'creator', key: 'creator' },
     { title: 'Date', dataIndex: 'createdAt', key: 'createdAt' },
-    { title: 'Action', key: 'operation', render: () => <a>Publish</a> },
+    { title: 'Action', key: 'operation', render: () => <a>Publish</a> }
   ];
 
   const data = [];
@@ -87,7 +87,7 @@ export default function NestedTable() {
       version: '10.3.4.5654',
       upgradeNum: 500,
       creator: 'Jack',
-      createdAt: '2014-12-24 23:12:00',
+      createdAt: '2014-12-24 23:12:00'
     });
   }
 
@@ -96,17 +96,11 @@ export default function NestedTable() {
       <Row>
         <Col className="hp-mb-16" lg={15} span={20}>
           <h4>Nested tables</h4>
-          <p className="hp-p1-body">
-            Showing more detailed info of every row.
-          </p>
+          <p className="hp-p1-body">Showing more detailed info of every row.</p>
         </Col>
 
         <Col lg={9} span={4} className="hp-text-right">
-          <Button
-            onClick={toggleChecked}
-            type="text"
-            icon={<RiCodeSSlashLine className="hp-text-color-black-80" />}
-          />
+          <Button onClick={toggleChecked} type="text" icon={<RiCodeSSlashLine className="hp-text-color-black-80" />} />
         </Col>
 
         <Col span={24}>
@@ -123,7 +117,7 @@ export default function NestedTable() {
       {checkedCode && (
         <SyntaxHighlighter
           language="javascript"
-          className={`show-code hp-mt-24 ${codeClass && "show-code-active"}`}
+          className={`show-code hp-mt-24 ${codeClass && 'show-code-active'}`}
           style={monoBlue}
         >
           {nested}
