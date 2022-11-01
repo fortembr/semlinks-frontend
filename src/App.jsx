f18a68bf09753fb61b5ae30464e8d7975aad5c8f
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 20:08:45 -0400

    delete languages

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,19 @@ import { useSelector } from 'react-redux';
 import { ConfigProvider } from 'antd';
 import { IntlProvider } from 'react-intl';
 
-import AppLocale from './languages';
-
 import Router from './router/Router';
 
 export default function App() {
   // Redux
   const customise = useSelector((state) => state.customise);
 
-  // Lang
-  const currentAppLocale = AppLocale[customise.language];
-
   useEffect(() => {
     document.querySelector('html').setAttribute('lang', customise.language);
   }, [customise]);
 
   return (
-    <ConfigProvider locale={currentAppLocale.antd} direction={customise.direction}>
-      <IntlProvider locale={currentAppLocale.locale} messages={currentAppLocale.messages}>
+    <ConfigProvider direction={customise.direction}>
+      <IntlProvider>
         <Router />
       </IntlProvider>
     </ConfigProvider>
