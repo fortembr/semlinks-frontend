aea54124f09462b1c01852101b3c79f4ed3633ff
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 23:32:04 -0400

    update component to Dashboard

--- src/view/main/layouts/page-layouts/full/index.jsx
+++ src/view/main/layouts/page-layouts/full/index.jsx
@@ -5,7 +5,7 @@ import Dashboard from '../../../dashboard';
 export default function PageLayoutsFull() {
   return (
     <div className="hp-p-32 hp-bg-black-20 hp-bg-dark-90">
-      <Analytics />
+      <Dashboard />
     </div>
   );
 }
