5eb215b0b9557947b4864c5a2ef2889a599c1590
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 23:26:28 -0400

    comment out HeaderNotifications

--- src/layout/components/header/index.jsx
+++ src/layout/components/header/index.jsx
@@ -96,7 +96,7 @@ export default function MenuHeader(props) {
 
             {/* <HeaderNotifications /> */}
 
-            <HeaderCart />
+            {/* <HeaderCart /> */}
 
             <HeaderUser />
           </Row>
