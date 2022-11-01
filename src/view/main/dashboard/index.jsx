6d34ee1026e05b05f444616f12b23cd48a132071
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 23:32:13 -0400

    delete useSelector

--- src/view/main/dashboard/index.jsx
+++ src/view/main/dashboard/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
-
 import { Row, Col } from 'antd';
 import { ExportSquare, ImportSquare, MoneyRecive, MoneySend, WalletMinus } from 'iconsax-react';
 
