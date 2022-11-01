5155d71dfa716d4b57b1eeb7ed308c0954dc8586
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 23:27:03 -0400

    lint file

--- src/layout/VerticalLayout.jsx
+++ src/layout/VerticalLayout.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import { useSelector } from 'react-redux';
-
 import { Layout, Row, Col } from 'antd';
 
 import Sidebar from './components/menu/Sidebar';
