397ce8f11e3fe6ed6b723aec923f2a7afe9dd9e7
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 23:28:01 -0400

    delete FullLayout

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -16,7 +16,6 @@ import { Routes } from './routes';
 
 // Layouts
 import VerticalLayout from '../layout/VerticalLayout';
-import FullLayout from '../layout/FullLayout';
 
 // Components
 // eslint-disable-next-line no-unused-vars
