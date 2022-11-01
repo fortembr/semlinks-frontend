83349250ab4dc7d3d9019aaafb718e2cefeff687
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 23:26:38 -0400

    delete CustomiseTheme

--- src/layout/VerticalLayout.jsx
+++ src/layout/VerticalLayout.jsx
@@ -7,7 +7,6 @@ import { Layout, Row, Col } from 'antd';
 import Sidebar from './components/menu/Sidebar';
 import MenuHeader from './components/header';
 import MenuFooter from './components/footer';
-import CustomiseTheme from './components/customise';
 import ScrollTop from './components/scroll-to-top';
 
 const { Content } = Layout;
