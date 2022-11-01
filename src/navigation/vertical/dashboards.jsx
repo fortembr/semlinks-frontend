3e123d7a44a09cd3997d470b0bfaf06eb9603c11
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 22:02:50 -0400

    delete unused links

--- src/navigation/vertical/dashboards.jsx
+++ src/navigation/vertical/dashboards.jsx
@@ -11,17 +11,6 @@ const main = [
     id: 'dashboard',
     title: 'Dashboard',
     icon: <Health size={18} />,
-  {
-    id: 'dashboards-ecommerce',
-    title: <IntlMessages id="sidebar-dashboards-ecommerce" />,
-    icon: <Setting size={18} />,
-    navLink: '/main/dashboard/ecommerce'
-  },
-  {
-    id: 'dashboards-nft',
-    title: <IntlMessages id="sidebar-dashboards-nft" />,
-    icon: <Grid5 size={18} />,
-    navLink: '/main/dashboard/nft'
     navLink: '/'
   }
   // {
