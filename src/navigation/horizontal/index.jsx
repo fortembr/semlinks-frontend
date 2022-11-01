692d303c4a1822e7525c18d71dedff52d7d566b5
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 22:01:24 -0400

    Delete index.jsx

--- src/navigation/horizontal/index.jsx
+++
@@ -1,8 +0,0 @@
-import dashboards from './dashboards';
-import apps from './apps';
-import pages from './pages';
-import userInterface from './user-interface';
-
-const navigation = [...dashboards, ...apps, ...pages, ...userInterface];
-
-export default navigation;
