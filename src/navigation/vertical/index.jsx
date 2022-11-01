c9501d388dd6c7e03010ac3600d6c1217cc81cf7
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-31 22:03:14 -0400

    import new menu sections

--- src/navigation/vertical/index.jsx
+++ src/navigation/vertical/index.jsx
@@ -1,4 +1,8 @@
 import dashboards from './dashboards';
+import localSEO from './localSEO';
+import offPageSEO from './offPageSEO';
+import onPageSEO from './onPageSEO';
+import technicalSEO from './technicalSEO';
 
 const navigation = [...dashboards, ...apps, ...pages, ...userInterface];
 
