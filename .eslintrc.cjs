5f58b9a3a91f12156823fa3a9c2c6e268ff7b205
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-11-05 23:08:06 -0400

    rename eslint file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,11 @@
 module.exports = {
   env: {
     node: true,
-    jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   overrides: [
     {
       files: ['*.js'], // Your JavaScript files extension
-      extends: ['plugin:prettier/recommended', 'plugin:react/recommended'],
-      parserOptions: {
-        sourceType: 'module',
-      },
-    },
-    {
-      files: ['/test/**/*.js'], // Your JavaScript files extension
       extends: ['plugin:prettier/recommended', 'plugin:react/recommended'],
       parserOptions: {
         sourceType: 'module',
