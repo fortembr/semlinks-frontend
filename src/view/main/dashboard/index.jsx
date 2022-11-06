1e66466f9d883e17129d54f0ff9a7941d29990e9
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-11-06 17:23:39 -0500

    clean page

--- src/view/main/dashboard/index.jsx
+++ src/view/main/dashboard/index.jsx
@@ -13,52 +13,17 @@ import HistoryCard from './historyCard';
 
 export default function Dashboard() {
   return (
-    <Row gutter={[32, 32]} className="hp-mb-32">
-      <Col flex="1" className="hp-overflow-hidden">
+    <Row
+      gutter={[32, 32]}
+      className='hp-mb-32'
+    >
+      <Col
+        flex='1'
+        className='hp-overflow-hidden'
+      >
         <Row gutter={[32, 32]}>
           <Col span={24}>
-            <h1 className="hp-mb-0">Dashboard</h1>
-          </Col>
-
-          <Col span={24}>
-            <Row gutter={[32, 32]}>
-              <Col sm={8} span={24}>
-                <FeatureCard
-                  icon={
-                    <MoneyRecive size="24" variant="Bold" className="hp-text-color-black-bg hp-text-color-dark-0" />
-                  }
-                  title="Income"
-                  titleIcon={<ExportSquare size="14" variant="Bold" className="hp-text-color-success-1" />}
-                  date="April 2022"
-                  price="$13,908"
-                />
-              </Col>
-
-              <Col sm={8} span={24}>
-                <FeatureCard
-                  icon={<MoneySend size="24" variant="Bold" className="hp-text-color-black-bg hp-text-color-dark-0" />}
-                  title="Expenses"
-                  titleIcon={<ImportSquare size="14" variant="Bold" className="hp-text-color-danger-1" />}
-                  date="April 2022"
-                  price="$7,949"
-                />
-              </Col>
-
-              <Col sm={8} span={24}>
-                <FeatureCard
-                  icon={
-                    <WalletMinus size="24" variant="Bold" className="hp-text-color-black-bg hp-text-color-dark-0" />
-                  }
-                  title="Balance"
-                  date="April 2022"
-                  price="$5,129"
-                />
-              </Col>
-            </Row>
-          </Col>
-
-          <Col span={24}>
-            <BalanceCard />
+            <h1 className='hp-mb-0'>Dashboard</h1>
           </Col>
 
           <Col span={24}>
@@ -71,7 +36,7 @@ export default function Dashboard() {
                   title: 'Product Designer',
                   price: '$ 7734.32',
                   number: '30432',
-                  percent: '23.23%'
+                  percent: '23.23%',
                 },
                 {
                   avatar: HistoryUser2,
@@ -80,7 +45,7 @@ export default function Dashboard() {
                   title: 'Product Designer',
                   price: '$ 7614.43',
                   number: '949',
-                  percent: '19.03%'
+                  percent: '19.03%',
                 },
                 {
                   avatar: HistoryUser3,
@@ -89,8 +54,8 @@ export default function Dashboard() {
                   title: 'Product Designer',
                   price: '$ 6789.19',
                   number: '732',
-                  percent: '13.98%'
-                }
+                  percent: '13.98%',
+                },
               ]}
             />
           </Col>
