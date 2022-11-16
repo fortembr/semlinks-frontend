f7efa6a42f3cafecb14e99f0ea82e55237a85c07
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-11-16 00:39:39 -0500

    setup basic login page

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -1,35 +1,26 @@
+// NPM Modules
+import { Box, Link, Typography } from '@mui/material';
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 
-import PublicTemplate from 'templates/public.template';
+// Custom Modules
+import { LoginForm } from 'molecules/forms';
+import { PublicTemplate } from 'templates';
+import logo from './../../src/assets/img/logo.png';
 
 export default class LoginPage extends React.Component {
   render() {
     return (
       <PublicTemplate>
-        <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
-          <Toolbar />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-            tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id
-            interdum velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl
-            suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras. Metus
-            vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt.
-            Cras tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-            lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+        <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, mt: 50 }}>
+          <img className="logo" src={logo} alt="SEM Links Logo" />
+          <Typography className="headline-primary" variant="h1">
+            Login
           </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla facilisi etiam
-            dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi
-            lacus sed viverra tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra
-            accumsan in. In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-            tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin nibh sit. Ornare
-            aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-            posuere sollicitudin aliquam ultrices sagittis orci a.
+          <LoginForm />
+          <Typography paragraph className="text-center m-20">
+            Don&apos;t have an account? <Link href="/register">Create an Account.</Link>
+            <br />
+            <Link href="/forgot-password">Forgot Password?</Link>
           </Typography>
         </Box>
       </PublicTemplate>
