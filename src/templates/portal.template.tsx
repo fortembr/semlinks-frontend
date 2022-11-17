cfff41b7e880e4876a1bc41818217c3efe100880
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-11-17 02:02:10 -0500

    add avatar menu to portal page template

--- src/templates/portal.template.tsx
+++ src/templates/portal.template.tsx
@@ -1,37 +1,135 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography
+} from '@mui/material';
+
 import InboxIcon from '@mui/icons-material/MoveToInbox';
+import { Logout, PersonAdd, Settings } from '@mui/icons-material';
 
 const drawerWidth = 240;
 
 interface PageProps {
   children: any;
+  pageTitle: string;
 }
 
 export default class PortalTemplate extends React.Component<PageProps> {
+  private isMenuOpen = false;
   constructor(props: PageProps) {
     super(props);
+    this.state = {};
   }
+  handleAccountMenuClick(event: React.MouseEvent<HTMLElement>) {
+    if (this.isMenuOpen) {
+      setAnchorEl(null);
+      return false;
+    }
+    setAnchorEl(event.currentTarget);
+    return true;
+  }
+
   render() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     return (
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
+        <AppBar position='fixed' sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
           <Toolbar>
-            <Typography variant="h6" noWrap component="div">
-              Permanent drawer
+            <Typography variant='h6' noWrap component='div'>
+              {this.props.pageTitle}
             </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+              <Tooltip title='Account settings'>
+                <IconButton
+                  id='avatar-icon'
+                  onClick={() => this.handleAccountMenuClick(event)}
+                  size='small'
+                  sx={{ ml: 2 }}
+                  aria-controls={this.isMenuOpen ? 'account-menu' : undefined}
+                  aria-haspopup='true'
+                  aria-expanded={this.isMenuOpen ? 'true' : undefined}
+                >
+                  <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                </IconButton>
+              </Tooltip>
+            </Box>
+            <Menu
+              anchorEl={anchorEl}
+              id='account-menu'
+              open={this.isMenuOpen}
+              onClose={() => this.handleAccountMenuClick(event)}
+              onClick={() => this.handleAccountMenuClick(event)}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: 'visible',
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1
+                  },
+                  '&:before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: 'background.paper',
+                    transform: 'translateY(-50%) rotate(45deg)',
+                    zIndex: 0
+                  }
+                }
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+              <MenuItem>
+                <Avatar /> Profile
+              </MenuItem>
+              <MenuItem>
+                <Avatar /> My account
+              </MenuItem>
+              <Divider />
+              <MenuItem>
+                <ListItemIcon>
+                  <PersonAdd fontSize='small' />
+                </ListItemIcon>
+                Add another account
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <Settings fontSize='small' />
+                </ListItemIcon>
+                Settings
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <Logout fontSize='small' />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
         <Drawer
@@ -43,14 +141,14 @@ export default class PortalTemplate extends React.Component<PageProps> {
               boxSizing: 'border-box'
             }
           }}
-          variant="permanent"
-          anchor="left"
+          variant='permanent'
+          anchor='left'
         >
           <Toolbar />
           <Divider />
           <List>
             <ListItem key={'Inbox'} disablePadding>
-              <ListItemButton component="a" href="#simple-list">
+              <ListItemButton component='a' href='#simple-list'>
                 <ListItemIcon>
                   <InboxIcon />
                 </ListItemIcon>
@@ -58,11 +156,11 @@ export default class PortalTemplate extends React.Component<PageProps> {
               </ListItemButton>
             </ListItem>
             <ListItem key={'Spam'} disablePadding>
-              <ListItemButton component="a" href="#simple-list">
+              <ListItemButton component='a' href='#simple-list'>
                 <ListItemIcon>
                   <InboxIcon />
                 </ListItemIcon>
-                <ListItemText primary="Spam" />
+                <ListItemText primary='Spam' />
               </ListItemButton>
             </ListItem>
           </List>
