af5221860aafbda8c8a1e2f7d31d323a09f5e0a1
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-11-16 20:02:35 -0500

    add RegisterPage to barrel export

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
-export { default as PortalPage } from './portal.page';
 export { default as LoginPage } from './login.page';
+export { default as PortalPage } from './portal.page';
+export { default as RegisterPage } from './register.page';
