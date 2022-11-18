c0de04d4f63999634adc73b8509d96e9f6d34763
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-11-17 22:34:23 -0500

    update button text

--- src/templates/portal.template.tsx
+++ src/templates/portal.template.tsx
@@ -117,7 +117,7 @@ export default class PortalTemplate extends React.Component<PageProps, PageState
               anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
             >
               <MenuItem>
-                <Avatar /> My account
+                <Avatar /> My Account
               </MenuItem>
               <Divider />
               <MenuItem>
